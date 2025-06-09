54519f630db3015fe049b998d57b48dd6dda84ca
Tanvir Alam Syed <tanviralamsyed@gmail.com>
2025-06-09 13:02:23 +0100

    new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function HomePage() {
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <h1 className="text-xl text-gray-700">Redirecting...</h1>
+      <h1 className="text-xl text-gray-700">Redirecting....</h1>
     </main>
   );
 }
